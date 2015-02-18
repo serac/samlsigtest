ad17300467aa1d8e3a3cd941802215e7ca871589
Marvin S. Addison <marvin.addison@gmail.com>
2015-02-18 09:05:27 -0500

    Use a schema-validating document builder.

--- src/main/java/edu/vt/middleware/SamlSignTest.java
+++ src/main/java/edu/vt/middleware/SamlSignTest.java
@@ -3,13 +3,24 @@ package edu.vt.middleware;
 import org.apache.commons.codec.binary.Base64InputStream;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.crypto.dsig.XMLSignature;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.dom.DOMValidateContext;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
@@ -17,10 +28,8 @@ import java.security.cert.X509Certificate;
  * SAML digital signature validation test.
  *
  */
-public class SamlSignTest
-{
-    public static void main(String[] args) throws Exception
-    {
+public class SamlSignTest {
+    public static void main(String[] args) throws Exception {
         if (args.length < 2) {
             System.out.println("USAGE: SamlSignTest path/to/response.b64 path/to/verify.crt");
             return;
@@ -29,9 +38,8 @@ public class SamlSignTest
         File certFile = new File(args[1]);
         boolean valid;
         try {
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            documentBuilderFactory.setNamespaceAware(true);
-            Document doc = documentBuilderFactory.newDocumentBuilder().parse(
+            DocumentBuilder builder = getSAMLDocumentBuilder();
+            Document doc = builder.parse(
                     new Base64InputStream(new FileInputStream(responseFile)));
                     CertificateFactory cf = CertificateFactory.getInstance("X.509");
             doc.getDocumentElement().setIdAttribute("ID", true);
@@ -51,5 +59,35 @@ public class SamlSignTest
             valid = false;
         }
         System.out.println("Signature valid? " + (valid ? "Yes" : "No"));
+    }
+
+    private static Source[] readSchemas() {
+        Source[] sources = null;
+        try {
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            sources = new Source[] {
+                    new StreamSource(new InputStreamReader(cl.getResourceAsStream("saml-schema-assertion-2.0.xsd"), "UTF-8")),
+                    new StreamSource(new InputStreamReader(cl.getResourceAsStream("saml-schema-protocol-2.0.xsd"), "UTF-8")),
+                    new StreamSource(new InputStreamReader(cl.getResourceAsStream("xmldsig-core-schema.xsd"), "UTF-8")),
+                    new StreamSource(new InputStreamReader(cl.getResourceAsStream("xenc-schema.xsd"), "UTF-8")) };
+        } catch (Exception e) {
+
+        }
+        return sources;
+
+    }
+
+    private static DocumentBuilder getSAMLDocumentBuilder() throws SAXException, ParserConfigurationException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setNamespaceAware(true);
+
+        Source[] sources = readSchemas();
+        if (sources != null) {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(sources);
+            documentBuilderFactory.setSchema(schema);
+        }
+
+        return documentBuilderFactory.newDocumentBuilder();
     }
 }
